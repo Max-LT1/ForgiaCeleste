edd76b76ecdf327c10589e52a4532764361cec24
Luca Giammattei <106800749+Sestimo117@users.noreply.github.com>
2026-09-13 15:40:29 +0200

    Fix servlet profilo

--- src/main/java/Control/Serv_profile.java
+++ src/main/java/Control/Serv_profile.java
@@ -17,7 +17,7 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet({"/Profilo", "/modificaUtente"})
+@WebServlet({"/modificaUtente"})
 public class Serv_profile extends HttpServlet {
     private static final long serialVersionUID = 5L;
     private ClienteDAO clienteDAO;
@@ -35,96 +35,12 @@ public class Serv_profile extends HttpServlet {
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        String servletPath = request.getServletPath();
-
-        switch(servletPath){
-            case "/modificaUtente":
-                try {
-                    modificaUtente(request, response);
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-                break;
-            case "/Profilo":
-                try {
-                    Profile(request, response);
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-                break;
-        }
-    }
-
-
-    public void Profile(HttpServletRequest req, HttpServletResponse res)
-            throws SQLException, ServletException, IOException{
-        String nome = req.getParameter("nome");
-        String cognome = req.getParameter("cognome");
-        String indirizzo = req.getParameter("indirizzo");
-        String citta = req.getParameter("citta");
-        String provincia = req.getParameter("provincia");
-        String cap = req.getParameter("cap");
-
-        if (!nome.matches("^[a-zA-Z]{1,50}$")) {
-            String errorMessage = "Invalid nome (1-50 characters)";
-            res.sendError(HttpServletResponse.SC_BAD_REQUEST, errorMessage);
-            return;
-        }
-        if (!cognome.matches("^[a-zA-Z]{1,50}$")) {
-            String errorMessage = "Invalid cognome (1-50 characters)";
-            res.sendError(HttpServletResponse.SC_BAD_REQUEST, errorMessage);
-            return;
-        }
-
-        // Validate indirizzo
-        if (!indirizzo.matches("^[a-zA-Z0-9 ]{1,100}$")) {
-            String errorMessage = "Invalid indirizzo (1-100 characters)";
-            res.sendError(HttpServletResponse.SC_BAD_REQUEST, errorMessage);
-            return;
-        }
-
-        // Validate citta
-        if (!citta.matches("^[a-zA-Z]{1,50}$") || !provincia.matches("^[a-zA-Z]{1,50}$")) {
-            String errorMessage = "Invalid indirizzo (1-50 characters)";
-            res.sendError(HttpServletResponse.SC_BAD_REQUEST, errorMessage);
-            return;
-        }
-
-        // Get the cliente ID from the session
-        HttpSession session = req.getSession();
-        Client cliente = ((Client) session.getAttribute("cliente"));
-
-        if (cliente == null) {
-            // Cliente is not authenticated, redirect to login page or show an error message
-            res.sendRedirect("login.jsp");
-            return;
-        }
-
-        // Create a new Cliente object
-
-        cliente.setUsername(cliente.getUsername());
-        cliente.setEmail(cliente.getEmail());
-        cliente.setNome(nome);
-        cliente.setCognome(cognome);
-        cliente.setIndirizzo(indirizzo);
-        cliente.setCitta(citta);
-        cliente.setCap(cap);
-        cliente.setProvincia(provincia);
-
         try {
-            // Update the cliente details in the database
-            clienteDAO.updateCliente(cliente);
-            // Update the cliente object in the session
-            session.setAttribute("cliente", cliente);
-            // Redirect to the profile page with a success message
-            res.sendRedirect("ProfiloUtente.jsp");
+            modificaUtente(request, response);
         } catch (SQLException e) {
-            // Redirect to the profile page with an error message
-            res.sendRedirect("ProfiloUtente.jsp");
+            throw new RuntimeException(e);
         }
     }
-
 
     public void modificaUtente(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException, SQLException {
@@ -132,14 +48,13 @@ public class Serv_profile extends HttpServlet {
         String nome = req.getParameter("nome");
         String cognome = req.getParameter("cognome");
         String username = req.getParameter("username");
-        String email = req.getParameter("currentEmail");
+        String email = req.getParameter("email");
         String indirizzo = req.getParameter("indirizzo");
         String citta = req.getParameter("citta");
         String provincia = req.getParameter("provincia");
         String Ogpsw = req.getParameter("currentPassword");
         String newpsw = req.getParameter("newPassword");
         String confermapsw = req.getParameter("confirmNewPassword");
-
 
         Client cliente = clienteDAO.getClienteByUsername(originalUsername);
         if(Password.check(Ogpsw, cliente.getPassword()).withArgon2()){
@@ -155,7 +70,7 @@ public class Serv_profile extends HttpServlet {
             }
             try {
                 clienteDAO.updateCliente(cliente);
-                req.getRequestDispatcher("index.jsp").forward(req, res);
+                req.getRequestDispatcher("HomePage").forward(req, res);
             } catch (SQLException e) {
                 res.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
             }
